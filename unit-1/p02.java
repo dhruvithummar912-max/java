a7ebc8f2fa989742090f9716a697159da6c2b52a
dhruvithummar912-max <dhruvithummar912@gmail.com>
2026-07-23 11:54:32 +0530

    Add files via upload

---
+++ unit-1/p02.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class p02
+{
+   	public static void main(String[] args)
+	{
+		Scanner a = new Scanner(System.in);
+		System.out.println("enter a number:");
+		int num = a.nextInt();
+		System.out.println("Answer"+num);
+		a.close();
+				
+
+
+}
+}
