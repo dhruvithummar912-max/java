43ab361517ed891cc0ec911b0af33f93447013dc
DHRUVI THUMMAR <dhruvithummar912@gmail.com>
2026-09-11 12:07:32 +0530

    Add files via upload

---
+++ unit-3/pro24.java
@@ -0,0 +1,18 @@
+public class pro24 {
+    public static void main(String[] args) {
+        try {
+            int a = 10;
+            int b = 0;
+
+            int result = a / b;
+
+            System.out.println("Result = " + result);
+        }
+        catch (ArithmeticException e) {
+            System.out.println("Exception: Cannot divide by zero.");
+        }
+        finally {
+            System.out.println("Finally block is always executed.");
+        }
+    }
+}
